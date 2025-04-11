4df1d03e77af2e7c4a6201153c978cb95e7aff12
uswahalvi <uswaaalvi@gmail.com>
2025-04-10 18:17:58 -0700

    add profile ui

--- frontend/src/features/userDashboard/UserProfile.jsx
+++ frontend/src/features/userDashboard/UserProfile.jsx
@@ -1,21 +1,95 @@
 import styled from "styled-components";
 import { useUser } from "../login/useUser";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { logoutUser, setUser } from "../login/loginSlice";
 import { useLogout } from "../login/useLogout";
 import Container from '../../ui/Container'
+import { FaUserCircle } from 'react-icons/fa';
 import Header from "../../ui/Header";
-
-const Button = styled.button`
-  width: 100px;
-`
+import addImg from '../../assets/add-image.png'
+import magicRing from '../../assets/magicRing.svg'
+const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-inline: 10px;
+  border-radius:10px;
+`;
+const CoverImageContainer = styled.div`
+  width: 100%;
+  height: 180px;
+  color: grey;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid grey;
+  border-radius:10px 10px 0px 0px;
+`;
+const CoverImg = styled.img`
+  border-radius:10px 10px 0px 0px;
+`;
+const ProfileHeader = styled.div`
+  display: flex;
+  position: relative;
+  margin: 20px;
+  border: 0.5px solid black;
+  padding: 100px 20px 10px;
+`;
+const ProfileImageContainer = styled.div`
+  position: absolute; 
+  border: 1px solid;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 47%;
+  left: 8%;
+  background: antiquewhite;
+  cursor: pointer;
+`;
+const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+`;
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 24px;
+  font-weight: 500;
+  flex: 3;
+  align-items: center;
+`;
+const FollowersContainer = styled.div`
+`;
+const PostsNumContainer = styled.div`
+`;
+const Name = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  text-transform: capitalize;
+`;
+const SkillLevelContainer = styled.div`
+  font-size: 20px;
+`;
+const ItemsContainer = styled.div`
+`;
 
 function UerProfile() {
   //testing useUser hook works fetching logged in user and geting the data?
   const userDetails = useSelector((store) => store.user);
   const isEffectRun = useRef(false);
+  const [followers, setFollowers] = useState(0)
+  const [following,setFollowing] = useState(0)
+  const [numOfPosts, setNumOfPosts] = useState(0)
+  const [profileImage, setProfileImage] = useState('')
+  const [coverImage, setCoverImage] = useState('')
+  const profileInputRef = useRef(null);
+  const coverInputRef = useRef(null);
+  
   const dispatch = useDispatch();
   const { logout } = useLogout();
   const navItems = [
@@ -25,7 +99,37 @@ function UerProfile() {
   ];
 
   const { isLoading, refetch } = useUser();
+  function handleProfileImageChange(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfileImage(reader.result); // set base64 image
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  function handleCoverImageChange(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setCoverImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
+  const handleProfileClick = () => {
+    profileInputRef.current?.click();
+  };
+  
+  const handleCoverClick = () => {
+    coverInputRef.current?.click();
+  };
+  
+  
   useEffect(() => {
     if (isEffectRun.current) return; // Prevent redundant calls
     isEffectRun.current = true;
@@ -54,18 +158,68 @@ function UerProfile() {
     }
 
   if (isLoading) return <p>Loading user data...</p>;
-  const { name, gender, email } = userDetails.userDetail;
-
+  const { name, gender, email, skillLevel } = userDetails.userDetail;
   return (
     <Container>
       <Header navItems={navItems}/>
-      <h4>Name</h4>
-      <p>{name}</p>
-      <h4>gender</h4>
-      <p>{gender}</p>
-      <h4>Email</h4>
-      <p>{email}</p>
-      <Button onClick={handleLogout}>Logout</Button>
+      <Profile>
+        {/* Hidden Inputs */}
+          <input
+            type="file"
+            accept="image/*"
+            ref={coverInputRef}
+            onChange={handleCoverImageChange}
+            style={{ display: 'none' }}
+          />
+          <CoverImageContainer onClick={handleCoverClick}>
+            {coverImage ? (
+              <CoverImg src={coverImage} alt="cover" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            ) : (
+              <>
+                <img src={addImg} width={45} />
+                <div>Click to add a cover photo</div>
+              </>
+            )}
+          </CoverImageContainer>
+        <ProfileHeader>
+          <input
+            type="file"
+            accept="image/*"
+            ref={profileInputRef}
+            onChange={handleProfileImageChange}
+            style={{ display: 'none' }}
+          />
+          <ProfileImageContainer onClick={handleProfileClick}>
+            {profileImage ? (
+              <img
+                src={profileImage}
+                alt="profile"
+                style={{ width: 135, height: 135, borderRadius: '50%', objectFit: 'cover' }}
+              />
+            ) : (
+              <FaUserCircle size={135} color="#333" />
+            )}
+          </ProfileImageContainer>
+          <LeftContainer>
+              <Name>{name}</Name>
+              <SkillLevelContainer>{skillLevel}</SkillLevelContainer>
+          </LeftContainer>
+          <RightContainer>
+                <FollowersContainer>
+                    {followers} Followers | {following} Following
+                </FollowersContainer>
+                <PostsNumContainer>
+                    {numOfPosts} Posts
+                </PostsNumContainer>
+          </RightContainer>
+        </ProfileHeader>
+        <ItemsContainer>
+
+        </ItemsContainer>
+        <ItemsContainer>
+          
+        </ItemsContainer>
+      </Profile>
     </Container>
   );
 }
