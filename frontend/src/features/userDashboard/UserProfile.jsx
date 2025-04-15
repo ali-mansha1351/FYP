16a989c52359870b8f158de68c7a2dbd93047a87
uswahalvi <uswaaalvi@gmail.com>
2025-04-14 17:36:38 -0700

    profile ui

--- frontend/src/features/userDashboard/UserProfile.jsx
+++ frontend/src/features/userDashboard/UserProfile.jsx
@@ -5,27 +5,38 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { logoutUser, setUser } from "../login/loginSlice";
 import { useLogout } from "../login/useLogout";
-import Container from '../../ui/Container'
 import { FaUserCircle } from 'react-icons/fa';
 import Header from "../../ui/Header";
 import addImg from '../../assets/add-image.png'
 import magicRing from '../../assets/magicRing.svg'
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const Profile = styled.div`
   display: flex;
   flex-direction: column;
   margin-inline: 10px;
   border-radius:10px;
+  background-color: var(--primary-color);
+  width: 75vw;
+  padding-bottom: 10px;
+  border: 1px solid ;
 `;
 const CoverImageContainer = styled.div`
   width: 100%;
-  height: 180px;
+  height: 150px;
   color: grey;
   cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid grey;
+  border-bottom: 1px solid var(--secondary-color);
   border-radius:10px 10px 0px 0px;
+  box-sizing: border-box;
+  &:hover{
+  
+  }
 `;
 const CoverImg = styled.img`
   border-radius:10px 10px 0px 0px;
@@ -34,7 +45,7 @@ const ProfileHeader = styled.div`
   display: flex;
   position: relative;
   margin: 20px;
-  border: 0.5px solid black;
+  border: 0.5px solid var(--secondary-color);
   padding: 100px 20px 10px;
 `;
 const ProfileImageContainer = styled.div`
@@ -44,8 +55,7 @@ const ProfileImageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  bottom: 47%;
-  left: 8%;
+  bottom: 50%;
   background: antiquewhite;
   cursor: pointer;
 `;
@@ -74,9 +84,60 @@ const Name = styled.div`
 `;
 const SkillLevelContainer = styled.div`
   font-size: 20px;
+  text-transform: capitalize;
 `;
 const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border: 1px solid var(--secondary-color);
 `;
+const BottomButton = styled.div`
+  font-size: 20px;
+  cursor: pointer;
+  width: fit-content;
+  padding-inline: 90px;
+  padding-block: 5px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  align-self: center;
+  background-color: var(--secondary-color);
+  border: 2px solid var(--secondary-color);
+  &:hover{
+    border: 2px solid black;
+  }
+`;
+const SubContainer= styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-inline: 20px;
+`;
+const Label = styled.div`
+  font-size: 20px;
+  width: fit-content;
+  padding-inline: 15px;
+  padding-block: 5px;
+  border: 0.5px solid var(--secondary-color);
+  border-bottom: none;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+const GridItem = styled.div`
+  padding: 20px;
+  text-align: center;
+  border-right: 1px solid var(--secondary-color);
+  border-bottom: 1px solid var(--secondary-color);
+
+  // remove right border on every 3rd item
+  &:nth-child(3n) {
+    border-right: none;
+  }
+
+  // remove bottom border on last row items
+  &:nth-last-child(-n + 3) {
+    border-bottom: none;
+  }
+`;
+
 
 function UerProfile() {
   //testing useUser hook works fetching logged in user and geting the data?
@@ -189,18 +250,18 @@ function UerProfile() {
             onChange={handleProfileImageChange}
             style={{ display: 'none' }}
           />
-          <ProfileImageContainer onClick={handleProfileClick}>
-            {profileImage ? (
-              <img
-                src={profileImage}
-                alt="profile"
-                style={{ width: 135, height: 135, borderRadius: '50%', objectFit: 'cover' }}
-              />
-            ) : (
-              <FaUserCircle size={135} color="#333" />
-            )}
-          </ProfileImageContainer>
           <LeftContainer>
+              <ProfileImageContainer onClick={handleProfileClick}>
+              {profileImage ? (
+                <img
+                  src={profileImage}
+                  alt="profile"
+                  style={{ width: 135, height: 135, borderRadius: '50%', objectFit: 'cover' }}
+                />
+              ) : (
+                <FaUserCircle size={135} color="#333" />
+              )}
+              </ProfileImageContainer>
               <Name>{name}</Name>
               <SkillLevelContainer>{skillLevel}</SkillLevelContainer>
           </LeftContainer>
@@ -213,15 +274,41 @@ function UerProfile() {
                 </PostsNumContainer>
           </RightContainer>
         </ProfileHeader>
-        <ItemsContainer>
-
-        </ItemsContainer>
-        <ItemsContainer>
-          
-        </ItemsContainer>
+        <SubContainer>
+          <Label>
+            Saved Items
+          </Label>
+          <ItemsContainer>
+              <GridItem>1</GridItem>
+              <GridItem>2</GridItem>
+              <GridItem>3</GridItem>
+              <GridItem>4</GridItem>
+              <GridItem>5</GridItem>
+              <GridItem>6</GridItem>
+          </ItemsContainer>
+          <BottomButton>
+              Click to see more
+          </BottomButton>
+        </SubContainer>
+        <SubContainer>
+          <Label>
+            Your Posts
+          </Label>
+          <ItemsContainer>
+              <GridItem>1</GridItem>
+              <GridItem>2</GridItem>
+              <GridItem>3</GridItem>
+              <GridItem>4</GridItem>
+              <GridItem>5</GridItem>
+              <GridItem>6</GridItem>
+          </ItemsContainer>
+          <BottomButton>
+              Click to see more
+          </BottomButton>
+        </SubContainer>
       </Profile>
     </Container>
-  );
+  )
 }
 
 //loader function
